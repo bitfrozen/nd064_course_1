3e68aad8d5edec35fdb3b426966ede8918621337
Antanas Domarkas <antanas.domarkas@curonia.lt>
2021-05-30 15:35:25 +0300

    Add website with template

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("welcome.html",
+                           message="Message from variable")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
