798e204e20280d129f74ef0197841fc4f8e96a88
Antanas Domarkas <antanas.domarkas@curonia.lt>
2021-05-30 16:41:35 +0300

    adding rest api

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, jsonify
 from model import db
 app = Flask(__name__)
 
@@ -21,5 +21,18 @@ def record_view(index):
         abort(404)
 
 
+@app.route("/api/record/")
+def api_record_list():
+    return jsonify(db)
+
+
+@app.route("/api/record/<int:index>")
+def api_record_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
+
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
