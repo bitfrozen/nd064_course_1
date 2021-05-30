835af5bcea84879e01c9cc50805f81108afa5eea
Antanas Domarkas <antanas.domarkas@curonia.lt>
2021-05-30 18:21:49 +0300

    exercise solution

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,62 +1,21 @@
-from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
-from model import db, save_db
+from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template("welcome.html",
-                           records=db)
+    return "Hello World!"
 
 
-@app.route("/record/<int:index>")
-def record_view(index):
-    try:
-        record = db[index]
-        return render_template("record.html",
-                               record=record,
-                               index=index,
-                               max_index=len(db) - 1)
-    except IndexError:
-        abort(404)
+@app.route("/status")
+def api_status():
+    return {"result": "OK - healthy"}
 
 
-@app.route("/add_record", methods=["GET", "POST"])
-def add_record():
-    if request.method == "POST":
-        card = {"name": request.form['name'],
-                "surname": request.form['surname']}
-        db.append(card)
-        save_db()
-        return redirect(url_for('record_view', index=len(db) - 1))
-    else:
-        return render_template("add_record.html")
-
-
-@app.route("/delete_record/<int:index>", methods=["GET", "POST"])
-def delete_record(index):
-    try:
-        if request.method == "POST":
-            del db[index]
-            save_db()
-            return redirect(url_for('hello'))
-        else:
-            return render_template('delete_record.html', record=db[index])
-    except IndexError:
-        abort(404)
-
-
-@app.route("/api/record/")
-def api_record_list():
-    return jsonify(db)
-
-
-@app.route("/api/record/<int:index>")
-def api_record_detail(index):
-    try:
-        return db[index]
-    except IndexError:
-        abort(404)
+@app.route("/metrics")
+def api_metrics():
+    return {"data": {"UserCount": 140, "UserCountActive": 23}}
 
 
 if __name__ == "__main__":
