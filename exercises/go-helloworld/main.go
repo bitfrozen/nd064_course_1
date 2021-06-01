5edf181623b89b4a3db4912bde6b9721d8a7ff77
Antanas Domarkas <antanas.domarkas@curonia.lt>
2021-06-01 22:15:30 +0300

    adding updated go file and Dockerfile

--- exercises/go-helloworld/main.go
+++ exercises/go-helloworld/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World")
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received", r.Method, "at", r.URL)
+	lines, err := fmt.Fprintf(w, "Hello World")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Written", lines, "lines")
 }
 
 func main() {
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":6111", nil)
+	http.HandleFunc("/", helloWorld)
+	err := http.ListenAndServe(":6111", nil)
+	if err != nil {
+		return
+	}
 }
