ce9e4c35f6952efb4eb24d69d394b22323d0682b
Antanas Domarkas <antanas.domarkas@curonia.lt>
2021-06-20 23:57:12 +0300

    Fix formatting

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -15,11 +15,12 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     return post
 
@@ -31,9 +32,11 @@ def get_post_count():
     connection.close()
     return post_count
 
+
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
 
 # Define the main route of the web application 
 @app.route('/')
@@ -43,20 +46,23 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
+
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
@@ -69,8 +75,7 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
             connection.commit()
             connection.close()
 
@@ -105,6 +110,7 @@ def metrics():
 
     return response
 
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    app.run(host='0.0.0.0', port='3111')
